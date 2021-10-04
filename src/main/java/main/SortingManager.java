5ea28ae7126162d7f1e62290c4e37e97ab17e7d1
Bilal Ahmed <bahmed98uk@gmail.com>
2021-10-04 10:31:00 +0100

    Added log4j

--- src/main/java/main/SortingManager.java
+++ src/main/java/main/SortingManager.java
@@ -1,21 +1,36 @@
 package main;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import controller.SorterFactory;
 import controller.theArray;
 import view.DisplayManager;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SortingManager {
     public static void main(String[] args) {
         // generate random array of ints
+        int length = 0;
+        int[] array;
         Scanner scan = new Scanner(System.in);
-        System.out.println("How many numbers do you want in the array?");
-        int length = scan.nextInt();
-        int[] array = new int[length];
-        theArray.generateArray(array);
+        try {
+            System.out.println("How many numbers do you want in the array?");
+            length = scan.nextInt();
+        } catch (InputMismatchException i) {
+            System.err.println("You have to enter an integer! Please try again...");
+            log.error("Expected an int but User has input a String."); //Logger log called here
+            System.exit(1);
+        }
 
+        array = new int[length];
+        theArray.generateArray(array);
         DisplayManager.displayQuestions();
         int choice = scan.nextInt();
         SorterFactory.setSort(choice, array);
+        PropertyConfigurator.configure("src/log4j.properties");
     }
+
+    private static Logger log = Logger.getLogger("Array Sorter logger.");
 }
